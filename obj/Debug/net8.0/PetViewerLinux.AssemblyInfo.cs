4cacd477f472ceb4294ebe554ccddaebd35c0f7b
Niduka Akalanka <nidukaakalanka@hotmail.com>
2025-07-20 17:44:58 +0530

    Add animations

--- obj/Debug/net8.0/PetViewerLinux.AssemblyInfo.cs
+++ obj/Debug/net8.0/PetViewerLinux.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetViewerLinux")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbfeb6970ad610fe475112d462c1ccab337c9d7e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d7628d16d51d1a1fe45e2f9a39656ca490d49f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetViewerLinux")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetViewerLinux")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
